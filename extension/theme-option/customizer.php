71fd30514c483e6ac8e400e85a3792f2837d4312
lakhacdiep <diep.lk@beetechsoft.com>
2022-04-14 13:48:09 +0700

    option single student product

--- extension/theme-option/customizer.php
+++ extension/theme-option/customizer.php
@@ -585,12 +585,35 @@ new Code(
 );
 
 /*
- * Section Service Single
+ * Section Single Student Product
+ * */
+new Section(
+    'design_opt_single_student_product',
+    [
+        'title'    => esc_html__( 'Single Student Product', 'design' ),
+        'panel'    => 'design_opt_panel',
+        'priority' => 10,
+    ]
+);
+
+// Field select contact form
+new Select(
+    [
+        'settings'    => 'design_opt_single_student_product_contact',
+        'label'       => esc_html__( 'Select Contact Form', 'design' ),
+        'section'     => 'design_opt_single_student_product',
+        'default'     => '',
+        'choices'     => design_get_form_cf7(),
+    ]
+);
+
+/*
+ * Section Single Service
  * */
 new Section(
     'design_opt_service_single',
     [
-        'title'    => esc_html__( 'Service Single', 'design' ),
+        'title'    => esc_html__( 'Single Service', 'design' ),
         'panel'    => 'design_opt_panel',
         'priority' => 10,
     ]
--- single-student_product.php
+++ single-student_product.php
@@ -2,12 +2,29 @@
 get_header();
 
 get_template_part( 'template-parts/breadcrumbs/inc', 'breadcrumbs' );
+
+$contact_from = get_theme_mod('design_opt_single_student_product_contact', '');
 ?>
 
     <div class="site-single-course site-single-my-product">
         <div class="element-section pt-2 pb-5">
             <div class="container custom-container">
-                <?php get_template_part('template-parts/student-product/inc','gallery'); ?>
+                <?php
+                get_template_part('template-parts/student-product/inc','gallery');
+
+                if ( $contact_from ) :
+                ?>
+
+                    <div class="element-btn-modal-form mt-5">
+                        <div class="btn-box text-center">
+                            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#register-course">
+                                <span><?php esc_html_e('Đăng kí học ngay', 'design'); ?></span>
+                                <i class="fas fa-arrow-right"></i>
+                            </button>
+                        </div>
+                    </div>
+
+                <?php endif; ?>
             </div>
         </div>
 
@@ -24,5 +41,39 @@ get_template_part( 'template-parts/breadcrumbs/inc', 'breadcrumbs' );
         </div>
     </div>
 
+<?php if ( $contact_from ) :?>
+
+    <div class="modal fade modal-theme modal-course" id="register-course" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <div class="modal-header__left">
+                        <h5 class="modal-title">
+                            <?php esc_html_e('Đăng kí học ngay', 'design'); ?>
+                        </h5>
+
+                        <div class="note">
+                            <span class="note__label"><?php esc_html_e('Lưu ý:', 'design'); ?></span>
+
+                            <span class="note__text"><?php esc_html_e('Tất cả các lịch học đều được học vào buổi tối', 'design'); ?></span>
+                        </div>
+                    </div>
+
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <?php echo do_shortcode( '[contact-form-7 id="'.$contact_from.'"]' ); ?>
+                </div>
+
+                <div class="modal-footer">
+                    <?php design_content_modal_contact_footer(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
+endif;
+
 get_footer();
